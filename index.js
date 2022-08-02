afba45e4d09a9a6f923578362e13b9e66abac8f9
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-08-02 11:30:06 +0530

    1

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
-
-const {Schema,model}=mongoose;
-const UserSchema=new Schema({
-    username:String,
-    password:String,
-    age:Number,})
-
-const UserModel=model('User',UserSchema);
-
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.get('/signup',(req,res)=>{
-const {username,password,age}=req.body;
-const user =new UserModel({
-    username,age,password,})
-user.save()
+app.use(express.urlencoded({ extended: true }));
+const jwt = require("jsonwebtoken");
+const { Schema, model } = mongoose;
+const UserSchema = new Schema({
+  username: String,
+  password: String,
+  age: Number,
+});
 
-})
-mongoose.connect('mongodb://127.0.0.1:27017/web17').then(()=>{
-    app.listen(8080,()=>{
-        console.log('Server is running on port 8080');
-    })
-})
+const UserModel = model("User", UserSchema);
 
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+  const { username, password, age } = req.body;
+  const user = new UserModel({
+    username,
+    age,
+    password,
+  });
+  user.save();
+  return res.json({ message: "user created" });
+});
+
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username, password });
+  if (!user) {
+    return res.status(401).send("user not found");
+  }
+  const token = jwt.sign(
+    { username: user.username, age: user.age, id: user._id },
+    "dinesh"
+  );
+  return res.send({ message: "user found", token });
+});
+mongoose.connect("mongodb://127.0.0.1:27017/web17").then(() => {
+  app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+  });
+});
